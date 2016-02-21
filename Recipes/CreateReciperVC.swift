7c339edee2d3ce07761a5334fbda139d17cd262f
lylejover <lylejover2yahoo.com>
2016-02-22 02:34:11 +0800

    Part 3 Final.

---
+++ Recipes/CreateReciperVC.swift
@@ -0,0 +1,35 @@
+//
+//  CreateReciperVC.swift
+//  Recipes
+//
+//  Created by Lyle Christianne Jover on 22/02/2016.
+//  Copyright © 2016 Lyle Christianne Jover. All rights reserved.
+//
+
+import UIKit
+
+class CreateReciperVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
